213bc199b4fe67373d52c5076da7e938f841ec2f
NamePoet <y1813880702@163.com>
2021-06-13 08:58:54 +0800

    Hello World

---
+++ HELLO_WORLD/cmake-build-debug/CMakeFiles/HELLO_WORLD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HELLO_WORLD.dir/main.c.obj"
+  "HELLO_WORLD.exe"
+  "HELLO_WORLD.exe.manifest"
+  "HELLO_WORLD.pdb"
+  "libHELLO_WORLD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HELLO_WORLD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
