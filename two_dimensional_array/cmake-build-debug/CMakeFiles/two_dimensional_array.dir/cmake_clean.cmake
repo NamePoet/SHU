33c9e5f5dbeb4007f0d017302c5c4179cd8e316b
NamePoet <y1813880702@163.com>
2021-06-16 15:19:45 +0800

    Two-dimensional array

---
+++ two_dimensional_array/cmake-build-debug/CMakeFiles/two_dimensional_array.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/two_dimensional_array.dir/main.c.obj"
+  "libtwo_dimensional_array.dll.a"
+  "two_dimensional_array.exe"
+  "two_dimensional_array.exe.manifest"
+  "two_dimensional_array.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/two_dimensional_array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ two_dimensional_array/main.c
@@ -0,0 +1,97 @@
+/*
+tic-tac-toe游戏
+• 读⼊入⼀一个3X3的矩阵，矩阵中的数字为1表
+⽰示该位置上有⼀一个X，为0表⽰示为O
+• 程序判断这个矩阵中是否有获胜的⼀一⽅方，
+输出表⽰示获胜⼀一⽅方的字符X或O，或输出⽆无
+⼈人获胜
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    const int size = 3;
+    int board[size][size];
+    int i,j;
+    int numOfX;
+    int numOfO;
+    int result = -1; // -1:没人赢， 1：X赢，0：O赢
+
+// 读入矩阵, 三行三列，按行输入
+    for (i=0; i<size; i++) {
+        for (j=0; j<size; j++) {
+            scanf("%d", &board[i][j]);
+        }
+    }
+//检查行  
+    for (i=0; i<size && result ==-1; i++) {
+        numOfO = numOfX = 0;
+        for (j=0; j<size; j++) {
+            if ( board[i][j] == 1 ) {
+                numOfX ++;
+            } else {
+                numOfO ++;
+            }
+        }
+        if ( numOfO == size ) {
+            result = 0;
+        } else if ( numOfX == size ) {
+            result = 1;
+        }
+    }
+//检查列
+    for (j=0; j<size && result == -1 ; j++) {
+        numOfO = numOfX = 0;
+        for (i=0; i<size; i++) {
+            if (board[i][j] == 1) {
+                numOfX ++;
+            } else {
+                numOfO ++;
+            }
+        }
+        if ( numOfO == size ) {
+            result = 0;
+        } else if ( numOfX == size ) {
+            result = 1;
+        }
+    }
+//检查主对角线
+    numOfO = numOfX = 0;
+    for ( i=0; i<size; i++ ) {
+        if ( board[i][i] == 1 ) {
+            numOfX ++;
+        } else {
+            numOfO ++;
+        }
+    }
+    if ( numOfO == size ) {
+        result = 0;
+    } else if ( numOfX == size ) {
+        result = 1;
+    }
+
+//检查副对角线
+    numOfO = numOfX = 0;
+    for ( i=0; i<size; i++) {
+        if ( board[i][i+size-1] == 1 ) {
+            numOfX = 1;
+        } else {
+            numOfO = 0;
+        }
+    }
+    if ( numOfO == size ) {
+        result = 0;
+    } else if ( numOfX == size ) {
+        result = 1;
+    }
+//输出结果
+    if ( result == -1 ) {
+        printf("没人赢！");
+    } else if (result == 0) {
+        printf("O胜利");
+    } else {
+        printf("X胜利！");
+    }
+
+}
